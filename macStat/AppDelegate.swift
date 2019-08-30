a84713d7807cdde8cec2492466b290a58b3f7fb5
Abraham Tarverdi <abraham.tarverdi@workday.com>
2019-08-30 09:40:26 -0700

    Initial Commit

---
+++ macStat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macStat
+//
+//  Created by Abraham Tarverdi on 8/30/19.
+//  Copyright © 2019 Tarverdi Ent. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
